cbe2aaee01b828127e1305515d16bf12c072fb55
Saravana-PIKUP <saravana@pikup.co.in>
2024-11-09 00:04:28 +0530

    all fixed react build error version

--- src/pages/dashboard/admin/index.tsx
+++ src/pages/dashboard/admin/index.tsx
@@ -1,12 +1,13 @@
 // src/pages/dashboard/admin/index.tsx
 import AdminLayout from '../../../components/AdminLayout';
+import Image from 'next/image';
 
 const AdminIndex = () => {
   return (
     <AdminLayout>
       <div className="flex-grow p-10 bg-gray-100 min-h-screen font-sans flex flex-col items-center">
         <div className="text-center">
-          <img src="/assets/logo.png" alt="Ironman Fitness Studio Logo" className="mb-6 w-96 h-96 object-contain mx-auto" />
+          <Image src="$1" alt="$2" width={384} height={384} className="$3" />
           <h1 className="text-5xl font-extrabold text-gray-900 mb-6">Welcome to the Admin Dashboard</h1>
           <p className="text-xl text-gray-800 mb-8">Manage users, subscription plans, gym equipment, payments, and view important reports. The Admin Dashboard provides all the necessary tools for smooth gym management.</p>
           <p className="mt-4 text-lg text-gray-600">33/6, Pallavaram - Kundrathur Rd, Andan Kuppam, Kundrathur, Chennai, Kolathuvancheri, Tamil Nadu 600069</p>
